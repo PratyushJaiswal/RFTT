c3465e38fa25ab86b9e570a40f61d9e8db3a6dcc
PratyushJaiswal <Pratyush.jaiswal@softwaysolutions.com>
2016-01-08 12:24:59 +0530

    comit

--- RFTTGIT/src/main/java/login.java
+++ RFTTGIT/src/main/java/login.java
@@ -1,9 +1,23 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import config.constansts;
+
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
+
 import config.*;
 
-public class login {
+public class login extends OpenBrowser{
 @Test
-public login(){
-	
+public login() throws IOException{
+	FileInputStream fis = new FileInputStream(constansts.Path_OR);
+	  Properties p = new Properties();
+	  p.load(fis);
+	  
+	driver.findElement(By.xpath(p.getProperty("Login_Email_Address"))).sendKeys("daniel@yopmail.com");
+	driver.findElement(By.xpath(p.getProperty("Login_Password")));
 }
 }
